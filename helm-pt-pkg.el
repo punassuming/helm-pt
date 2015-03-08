3522e816be0a712cab2f61881c559c6dd6f793de
Rich Alesi <scio62@gmail.com>
2015-03-07 22:43:03 -0700

    Updated pkg config

--- helm-pt-pkg.el
+++ helm-pt-pkg.el
@@ -1,4 +1,6 @@
-(define-package "ahk-mode" "20150307.141210" "Helm interface to the platinum searcher"
-  'nil
+(define-package "helm-pt"
+  "20150307.141210"
+  "Helm interface to the platinum searcher"
+  '((helm "1.5.6"))
   :keywords '("helm" "platinum searcher")
   )
