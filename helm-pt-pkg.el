f3c3b6cb84faa4db2e38723ee41eec3b345dbc1f
Rich Alesi <scio62@gmail.com>
2015-03-07 22:15:19 -0700

    updated documentation and added package config

---
+++ helm-pt-pkg.el
@@ -0,0 +1,4 @@
+(define-package "ahk-mode" "20150307.141210" "Helm interface to the platinum searcher"
+  'nil
+  :keywords '("helm" "platinum searcher")
+  )
--- helm-pt.el
+++ helm-pt.el
@@ -25,7 +25,8 @@
 ;; a helm interface for the platinum searcher, allowing both ADHOC
 ;; running, and integrating into projectile.
 
-;; PT FOUNT AT : https://github.com/monochromegane/the_platinum_searcher
+;; the platinum searcher can be found at
+;; <https://github.com/monochromegane/the_platinum_searcher>
 
 ;;; Code:
 
